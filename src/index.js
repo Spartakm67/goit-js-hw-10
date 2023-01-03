28caf5fd13e22f81a677049771e280bbca793a3b
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-03 17:22:29 +0200

    v.01/03/2023

--- src/index.js
+++ src/index.js
@@ -3,12 +3,40 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import { fetchCountries }  from './js/fetchCountries';
+import Fetch from './js/fetchCountries';
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
+// const name = 'Au';    
+// Fetch.fetchCountries(name);
+const input = document.querySelector('#search-box');
 
-const name = 'blablabla'; 
-    
-fetchCountries(name);
+input.addEventListener('input', debounce(onInputValueHandler, DEBOUNCE_DELAY));
+
+function onInputValueHandler(e) {
+    e.preventDefault();
+    const name = e.target.value.trim();
+//   if (!name) {
+//     clearMarkup();
+//     return;
+//   }
+  
+    Fetch.fetchCountries(name)
+        .then(data => {
+            if (data.length > 10) {
+                Notify.info(
+                    'Too many matches found. Please enter a more specific name.'
+                );
+            } else if (data.length > 1 && data.length <= 10) {
+                // clearMarkup();
+                markupListOfCountries(data);
+            } else if (data.length === 1) {
+                // clearMarkupCounries()
+                markupOneCountryDetail(data);
+            }
+        })
+        .catch(Fetch.onFetchError)
+        .finally(clearMarkup);
+};
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,7 +3,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-export function  fetchCountries(name) {
+ function  fetchCountries(name) {
   fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
         (response) => {
             if (response.status === 404) {
@@ -15,7 +15,11 @@ export function  fetchCountries(name) {
 ).catch(() => Notify.failure('Oops, there is no country with that name'));
     };
 
+function onFetchError(error) {
+    Notify.failure('Oops, something wrong');
+}
 
+export default { fetchCountries, onFetchError };
 
 
 
