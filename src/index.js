36bd81723b98f714b26d2533e0befdef87ee3933
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-03 14:44:05 +0200

    Function  Fetch Added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
+
 import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { fetchCountries }  from './js/fetchCountries';
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+
+const name = 'blablabla'; 
+    
+fetchCountries(name);
